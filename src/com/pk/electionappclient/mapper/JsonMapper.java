dc78de9be0cd0fffc6c5db419d7f78d1021741a0
TomaszSiwiec <ts@onet.pl>
2019-12-19 10:09:40 +0100

    [TO13] Added HttpResponser, jsonMapper and ClientController

---
+++ src/com/pk/electionappclient/mapper/JsonMapper.java
@@ -0,0 +1,15 @@
+package com.pk.electionappclient.mapper;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.IOException;
+
+public class JsonMapper {
+
+    public static <T> T mapToObject(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        T object = objectMapper.readValue(json, new TypeReference<T>(){});
+        return object;
+    }
+}

02d993598e9b0c92a06a19f7759503ef4a899dcb
km <kamil.m@outlook.com>
2019-12-27 08:44:38 +0100

    backup

--- src/com/pk/electionappclient/Controller/Admin/NewCandidateController.java
+++ src/com/pk/electionappclient/Controller/Admin/NewCandidateController.java
@@ -1,14 +1,45 @@
 package com.pk.electionappclient.Controller.Admin;
 
 import com.pk.electionappclient.Controller.AppController;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class NewCandidateController extends AppController {
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewCandidateController extends AppController implements Initializable {
 
     @FXML
     Button exitButton;
+    @FXML
+    Button saveButton;
+    @FXML
+    TextField candidateSurnameTextField;
+    @FXML
+    TextField candidateNameTextField;
+    @FXML
+    TextField candidateCityTextField;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+//        saveButton.disableProperty().bind(
+//                Bindings.isEmpty(candidateSurnameTextField.textProperty())
+//                        .or(Bindings.isEmpty(candidateNameTextField.textProperty()))
+//                        .or(Bindings.isEmpty(candidateCityTextField.textProperty()))
+//        );
+        disableButton(saveButton, candidateSurnameTextField,candidateNameTextField,candidateCityTextField);
+    }
+
+    public void createNewCandidate(ActionEvent actionEvent) {
+
+    }
+
 
     public void closePanel(ActionEvent actionEvent) {
         closeLoginPanelOnAction(actionEvent, exitButton);
--- src/com/pk/electionappclient/Controller/AppController.java
+++ src/com/pk/electionappclient/Controller/AppController.java
@@ -1,8 +1,10 @@
 package com.pk.electionappclient.Controller;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -10,6 +12,20 @@ import java.util.Date;
 import java.util.List;
 
 public abstract class AppController {
+
+    public void disableButton(Button button, TextField... textFields) {
+        int flag = 0, counter = 0;
+        for (TextField textField : textFields) {
+            if (textField.getText().trim().isEmpty()) {
+                flag++;
+            }
+            counter++;
+        }
+
+        if (flag==counter) {
+            button.setDisable(false);
+        }
+    }
 
     public void closeLoginPanelOnAction(ActionEvent actionEvent, Button button) {
         Stage stage = (Stage) button.getScene().getWindow();
